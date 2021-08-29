b6a302c1f3a7facb06421f734782c32ae7b33d15
Fidie-e <elbouanani.f@gmail.com>
2021-08-29 17:34:26 +0200

    target button carousel ok

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,119 +1,128 @@
-// initialisation
-let currentSlideNumber = 0;
-let autoScrollTimer = null;
+const appCarousel = {
 
-// démarrage du timer
-reinitTimer();
+    currentSlideNumber: 0,
+    autoScrollTimer: null,
 
-// récupération de tous les boutons de compte des slides
-const allButtons = document.querySelectorAll('.number');
 
-// je pose un écouteur sur chaque bouton
-for (const button of allButtons) {
-    button.addEventListener('click', handleSpecificSlide);
-}
+    init: function() {
+        
+        // démarrage du timer
+        appCarousel.reinitTimer();
+        
+        // récupération de tous les boutons de compte des slides
+        const allButtons = document.querySelectorAll('.number');
+        
+        // je pose un écouteur sur chaque bouton
+        for (const button of allButtons) {
+            button.addEventListener('click', appCarousel.handleSpecificSlide);
+        }
+    },
 
-// afficher une slide spécifique
-function handleSpecificSlide(event) {
-    console.log('clic sur le bouton');
 
-    // déterminer et cibler le bouton le cliqué
-    const clickedButton = event.currentTarget;
+    // afficher une slide spécifique
+    handleSpecificSlide: function(event) {
+        console.log('clic sur le bouton');
 
-    // je cible l'attribut 'data-slide-number' (slideNumber) de l'élément
-    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
-    const targetSlideNumber = clickedButton.dataset.slideNumber;
+        // déterminer et cibler le bouton le cliqué
+        const clickedButton = event.currentTarget;
 
-    // je précise que la slide courante est la nouvelle slide
-    currentSlideNumber = targetSlideNumber;
+        // je cible l'attribut 'data-slide-number' (slideNumber) de l'élément
+        // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
+        const targetSlideNumber = clickedButton.dataset.slideNumber;
 
-    // déclenchement de l'affichage de la slide
-    displaySlide(targetSlideNumber);
+        // je précise que la slide courante est la nouvelle slide
+        appCarousel.currentSlideNumber = targetSlideNumber;
 
-    // réinitialision du timer de défilement auto à chaque clic
-    reinitTimer();
-}
+        // déclenchement de l'affichage de la slide
+        appCarousel.displaySlide(targetSlideNumber);
 
-// réinitialiser le timer
-function reinitTimer() {
+        // réinitialision du timer de défilement auto à chaque clic
+        appCarousel.reinitTimer();
+    },
 
-    // si on a une valeur dans autoScrollTimer, 
-    if (autoScrollTimer) {
 
-        // on supprime ce timer
-        clearInterval(autoScrollTimer);
+    // réinitialiser le timer
+    reinitTimer:function() {
+
+        // si on a une valeur dans autoScrollTimer, 
+        if (appCarousel.autoScrollTimer) {
+
+            // on supprime ce timer
+            clearInterval(appCarousel.autoScrollTimer);
+        }
+
+        // on crée un nouveau timer de défilement auto
+        appCarousel.autoScrollTimer = setInterval(appCarousel.displayNextSlide, 3000);
+    },
+
+
+    // afficher la slide suivante
+    displayNextSlide:function() {
+
+        // récupérer le numéro l'élément cible
+        appCarousel.getNextSlideNumber();
+        const nextSlideNumber = appCarousel.currentSlideNumber;
+        appCarousel.displaySlide(nextSlideNumber);
+    },
+
+
+    // affcher une slide en fonction de son numéro (en argument)
+    displaySlide:function(targetSlideNumber) {
+
+        const containerElement = document.querySelector('.carousel');
+
+        // récupérer la largeur du conteneur
+        const containerWidth = containerElement.offsetWidth;
+
+        // calcul de la distance de scroll à appliquer
+        const offsetX = containerWidth * targetSlideNumber;
+
+        // faire le scroll
+        containerElement.scrollTo(offsetX, 0);
+
+        // mettre à jour la barre de navigation
+        appCarousel.highlightCurrentSlideButton();
+    },
+
+
+    // récupérer le numéro de la prochaine slide
+    getNextSlideNumber:function() {
+
+        // compter le nombre de slides disponibles (ici 3)
+        const slidesCount = document.querySelectorAll('.carousel--item').length;
+        
+        // calcul du numéro de la slide suivante
+        // pour retourner à la première et sinon itérer
+        // (-1 car on commence à l'index 0)
+        if (appCarousel.currentSlideNumber == slidesCount - 1) {
+            appCarousel.currentSlideNumber = 0
+        } else {
+            appCarousel.currentSlideNumber++;
+        }
+    },
+
+
+    // activation du bouton correspondant à la slide affichée
+    highlightCurrentSlideButton:function() {
+
+        const allButtons = document.querySelectorAll('.number');
+
+        // récupérer et cibler le bouton à mettre en évidence
+        // en fontion de la current slide active 
+        // qui a l'attribut "data-slide-number" qui vaut 'currentSlideNumber'
+        // https://developer.mozilla.org/fr/docs/Learn/CSS/Building_blocks/Selectors/Attribute_selectors 
+        const slideNumber = '.number[data-slide-number="' + appCarousel.currentSlideNumber + '"]';
+        const targetButton = document.querySelector(slideNumber);
+        
+        // je boucle sur chaque bouton car je ne peux pas utiliser 'classList'
+        //  sur plusieurs éléments -> querySelectorAll
+        for (const button of allButtons) {
+            button.classList.remove('active');
+        }
+        
+        // appliquer la classe .active au bouton cible
+        targetButton.classList.add('active');
     }
-
-    // on crée un nouveau timer de défilement auto
-    autoScrollTimer = setInterval(displayNextSlide, 3000);
 }
 
-// afficher la slide suivante
-function displayNextSlide() {
-
-    // récupérer le numéro l'élément cible
-    const nextSlideNumber = getNextSlideNumber();
-    displaySlide(nextSlideNumber);
-}
-
-// affcher une slide en fonction de son numéro (en argument)
-function displaySlide(targetSlideNumber) {
-
-    const containerElement = document.querySelector('.carousel');
-
-    // récupérer la largeur du conteneur
-    const containerWidth = containerElement.offsetWidth;
-
-    // calcul de la distance de scroll à appliquer
-    const offsetX = containerWidth * targetSlideNumber;
-
-    // faire le scroll
-    containerElement.scrollTo(offsetX, 0);
-
-    // mettre à jour la barre de navigation
-    highlightCurrentSlideButton();
-}
-
-// récupérer le numéro de la prochaine slide
-function getNextSlideNumber() {
-
-    // compter le nombre de slides disponibles (ici 3)
-    const slidesCount = document.querySelectorAll('.carousel--item').length;
-    
-    // calcul du numéro de la slide suivante
-    // pour retourner à la première et sinon itérer
-    // (-1 car on commence à l'index 0)
-    if (currentSlideNumber == slidesCount - 1) {
-        currentSlideNumber = 0
-    } else {
-        currentSlideNumber++;
-    }
-
-    return currentSlideNumber;
-}
-
-// activation du bouton correspondant à la slide affichée
-function highlightCurrentSlideButton() {
-
-    // récupérer et cibler le bouton à mettre en évidence
-    // en fontion de la current slide active 
-    // qui a l'attribut "data-slide-number" qui vaut 'currentSlideNumber'
-    // https://developer.mozilla.org/fr/docs/Learn/CSS/Building_blocks/Selectors/Attribute_selectors 
-    const targetButton = document.querySelector('.number[data-slide-number="' + currentSlideNumber + '"]');
-
-    // retirer la classe .active à tous les boutons 'number'
-    const allButtons = document.querySelectorAll('.number');
-    
-    // je boucle sur chaque bouton car je ne peux pas utiliser 'classList'
-    //  sur plusieurs éléments -> querySelectorAll
-    for (const button of allButtons) {
-        button.classList.remove('active');
-    }
-    
-    // appliquer la classe .active au bouton cible
-    targetButton.classList.add('active');
-}
-
-
-
-
+document.addEventListener("DOMContentLoaded", appCarousel.init);
